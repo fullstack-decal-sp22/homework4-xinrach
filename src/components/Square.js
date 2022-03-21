71da065af8e336a2a322e101058da47b9c8de21e
Rachel Xin <rachelxin@berkeley.edu>
2022-03-20 23:51:03 -0700

    Finish proj

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,12 +1,12 @@
 import React from "react";
 import './styles/Square.css';
-
-function Square() {
-    return (
-        <button className="square">
-            {/* TODO */}
-        </button>
-    )
+ 
+function Square(props) {
+   return (
+       <button className="square" onClick={props.onClick}>
+           {props.value}
+       </button>
+   )
 }
-
+ 
 export default Square;
